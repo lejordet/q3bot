3c92a14531b9eac509723cf5aa38142d3950ad28
Lars Erik Jordet <lejordet@gmail.com>
2021-10-28 21:22:43 +0200

    Working bot/container parser combo

---
+++ q3bot.py
@@ -0,0 +1,193 @@
+import discord
+from dateutil.parser import parse
+import json
+import pytz
+import paho.mqtt.client as mqtt
+import logging
+from collections import deque
+from asyncio import sleep
+import random
+
+logging.basicConfig(filename="discord.log", level=logging.INFO, 
+     format= '[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
+     datefmt='%H:%M:%S')
+# set up logging to console
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
+
+logger = logging.getLogger(__name__)
+
+TZ = pytz.timezone("Europe/Oslo")
+IX_WORLD = "1022"
+STYLE_EMOJI = ['👻', '💀', '☠️', '😵', '🤯', '🤬', '🤘', '🎯', '💣', '🍖']
+
+class Q3Client(discord.Client):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.cfg = parse_config()
+        self.mqtt = mqtt.Client("q3client")
+        self.mqtt.on_connect = self.on_mqtt_connect
+        self.mqtt.on_message = self.on_mqtt_message
+        self.mqtt.on_log = self.on_mqtt_log
+
+        self.mqtt.connect(self.cfg["mqtt"])
+        # an attribute we can access from our task
+        self.clients = dict()
+        self.msgs = deque()
+        # create the background task and run it in the background
+        self.bg_task = self.loop.create_task(self.my_background_task())
+
+        self.current_game = dict()
+
+        self.mqtt.loop_start()
+
+    async def on_ready(self):
+        logger.info('Logged in as')
+        logger.info(self.user.name)
+        logger.info(self.user.id)
+        logger.info('------')
+
+    def on_mqtt_log(client,userdata,level,buff):
+        console.debug(buff)
+
+    def on_mqtt_connect(self, client, userdata, flags, rc):
+        logger.info("Connected with result code "+str(rc))
+        try:
+            client.subscribe("q3server/#", qos=2)
+        except Exception as details:
+            logger.exception("Subscription failed", exc_info=details)
+
+
+    def on_mqtt_message(self, client, userdata, msg):
+        tokens = msg.topic.split("/")
+        if tokens[0] != "q3server":
+            logger.info("q3client", msg.topic+" "+str(msg.payload))
+            return True
+    
+
+        if tokens[1] != "log":
+            return True
+
+
+        payload = msg.payload.decode('utf-8')
+        try:
+            payload = json.loads(payload)
+        except json.decoder.JSONDecodeError:
+            logger.error(f"{payload} isn't JSON")
+            return True
+
+        logstr = f"{msg.topic} {payload}"
+        logger.info(logstr)
+        
+        ts = parse(payload["timestamp"]).astimezone(TZ)
+        # This is the action!
+        if tokens[2] == "ShutdownGame":
+            self.clients = dict()
+            self.current_game = dict()
+            self.msgs.append(f"Server restarting at {ts:%Y-%m-%d %H:%M}!")
+        elif tokens[2] == "InitGame":
+            if any(self.clients):  # Only if players are connected
+                self.msgs.append(f"New game starting on {payload['mapname']} at {ts:%Y-%m-%d %H:%M}!")
+            self.current_game.update(payload)
+            self.current_game["fraglimit"] = int(self.current_game.get("fraglimit", 100))
+
+            self.clients = dict()
+        elif tokens[2] == "Exit":
+            self.msgs.append(f"Game ended due to {payload['reason'].lower()} at {ts:%Y-%m-%d %H:%M}")
+            self.current_game = dict()
+        elif tokens[2] == "Score":
+            self.msgs.append(f" > {payload['n']}: {payload['score']} kills")
+        elif tokens[2] == "Kill":
+            if payload["method"] == "MOD_LIGHTNING":
+                self.msgs.append(f"{payload['n']} killed {payload['targetn']} with ⚡ SHÆÆÆÆÆÆÆÆÆÆÆÆÆÆFT! ⚡")
+
+            if payload["clientid"] == IX_WORLD:   # falling damage, etc.
+                clidx = payload["targetid"]
+            else:
+                clidx = payload["clientid"]
+
+            cli = self.clients.setdefault(clidx, dict())
+            cli.setdefault("running_score", 0)
+
+            if clidx == payload["targetid"]:  # Suicide
+                cli["running_score"] -= 1
+            else:
+                cli["running_score"] += 1
+                if cli["running_score"] > 0 and (cli["running_score"] % 5) == 0:
+                    if "n" in cli:
+                        self.msgs.append(f"{cli['n']} has {cli['running_score']} kills")
+                delta = cli["running_score"] - self.current_game.get("fraglimit", 100)
+                style = random.choice(STYLE_EMOJI)
+                if delta == -3:
+                    self.msgs.append(f"THREE FRAGS LEFT {style*3}")
+                elif delta == -2:
+                    self.msgs.append(f"TWO FRAGS LEFT {style*2}")
+                elif delta == -1:
+                    self.msgs.append(f"ONE FRAG LEFT {style}")
+        elif tokens[2] == "Client":
+            clidx = payload["clientid"]
+            if not any(self.clients):
+                # New game!
+                self.msgs.append(f"Q3E server circuitry.no: New game starting on {self.current_game.get('mapname', '<unknown map>')} at {ts:%Y-%m-%d %H:%M}!")
+            cli = self.clients.setdefault(clidx, dict())
+            prev_name = cli.get('n')
+            cli.update(payload)
+            if payload["action"] == "Disconnect":
+                del self.clients[clidx]
+                serverstate = f"{len(self.clients)} players online" if any(self.clients) else "server empty"
+                self.msgs.append(f"{cli.get('n', '<unknown>')} disconnected, {serverstate}")
+            elif payload["action"] == "Begin":
+                pass  # we trigger on receiving the name instead
+            elif payload["action"] == "Connect":
+                pass
+            elif payload["action"] == "InfoChanged":
+                if prev_name is not None and prev_name != cli['n']:
+                    self.msgs.append(f"{prev_name} changed name to {cli.get('n', '<unknown>')}")
+            if prev_name is None and "n" in payload:
+                serverstate = f"{len(self.clients)} players online" if any(self.clients) else "server empty"
+                self.msgs.append(f"{cli.get('n', '<unknown>')} joined the game, {serverstate}")
+
+        return True
+
+    async def my_background_task(self):
+        await self.wait_until_ready()
+        print(self.cfg)
+        channel = self.get_channel(int(self.cfg["channel"]))
+        print(channel)
+        while not self.is_closed():
+            msg = None
+            try:
+                msg = self.msgs.popleft()
+            except IndexError:
+                await sleep(0.01)  # tiny sleep to avoid spamming CPU
+
+            if msg is not None:
+                await channel.send(msg)
+
+
+def parse_config():
+    cfg = dict()
+    with open("secrets.ini", "rt") as f:
+        for line in f.readlines():
+            k, v = line.strip().split("=")
+            cfg[k] = v
+    return cfg
+
+
+def main():
+    cfg = parse_config()
+    intents = discord.Intents.default()
+    intents.typing = False
+    intents.presences = False
+    client = Q3Client(intents=intents)
+
+    client.run(cfg["token"])
+
+
+if __name__ == "__main__":
+    main()
---
+++ q3container.py
@@ -0,0 +1,208 @@
+import docker
+from dateutil.parser import parse
+import paho.mqtt.client as mqtt
+import logging
+import json
+
+
+logging.basicConfig(filename="mqtt.log", level=logging.INFO, 
+     format= '[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s',
+     datefmt='%H:%M:%S')
+# set up logging to console
+console = logging.StreamHandler()
+console.setLevel(logging.DEBUG)
+# set a format which is simpler for console use
+formatter = logging.Formatter('%(name)-12s: %(levelname)-8s %(message)s')
+console.setFormatter(formatter)
+# add the handler to the root logger
+logging.getLogger('').addHandler(console)
+
+logger = logging.getLogger(__name__)
+
+
+MQTTSERVER = "localhost"
+DCK = docker.from_env()
+SHUTDOWN = False
+
+
+MEANS_OF_DEATH = {
+    0:"MOD_UNKNOWN",
+    1:"MOD_SHOTGUN",
+    2:"MOD_GAUNTLET",
+    3:"MOD_MACHINEGUN",
+    4:"MOD_GRENADE",
+    5:"MOD_GRENADE_SPLASH",
+    6:"MOD_ROCKET",
+    7:"MOD_ROCKET_SPLASH",
+    8:"MOD_PLASMA",
+    9:"MOD_PLASMA_SPLASH",
+    10:"MOD_RAILGUN",
+    11:"MOD_LIGHTNING",
+    12:"MOD_BFG",
+    13:"MOD_BFG_SPLASH",
+    14:"MOD_WATER",
+    15:"MOD_SLIME",
+    16:"MOD_LAVA",
+    17:"MOD_CRUSH",
+    18:"MOD_TELEFRAG",
+    19:"MOD_FALLING",
+    20:"MOD_SUICIDE",
+    21:"MOD_TARGET_LASER",
+    22:"MOD_TRIGGER_HURT",
+    23:"MOD_GRAPPLE"
+}
+
+def handle_log():
+    q3 = DCK.containers.get("quake3e_ded")
+    logger.info(f"Following container {q3}")
+    for line in q3.logs(tail=100, follow=True, stream=True, timestamps=True):
+        yield line.decode('utf-8').strip()
+
+
+def parse_combined_line(line):
+    line = " ".join(line)
+    skip = 1 if line[0] == "\\" else 0
+    fixed = line[skip:].split("\\")
+    keys = fixed[::2]
+    values = fixed[1::2]
+
+    return dict(zip(keys, values))
+
+
+def parse_scores(line):
+    obj = dict()
+    # line looks like "score: NUM ping: NUM client: ID NAME".split(" ")
+    i = 0
+    while i < len(line):
+        if line[i] == "score:":
+            obj["score"] = int(line[i+1])
+        elif line[i] == "ping:":
+            obj["ping"] = int(line[i+1])
+        elif line[i] == "client:":
+            obj["clientid"] = line[i+1]
+            obj["n"] = line[i+2]
+        i += 1
+
+    return obj
+
+def parse_line(line):
+    tokens = line.split(" ")
+    ts = parse(tokens[0])
+    buildobj = {"timestamp": tokens[0], "line": line}
+    if tokens[1][-1] != ":":
+        buildobj["action"] = "info"
+        return None  # shortcut
+
+    action = tokens[1][:-1]
+
+    kval = {"timestamp": tokens[0], "line": line}
+    buildobj["action"] = action
+    if len(tokens) < 3:
+        return buildobj
+
+    idx = tokens[2]  # might not be an index, we'll figure it out
+    if action in ("Item", "Hunk_Clear", "IP",):
+        return None
+    elif action == "ClientConnect":
+        buildobj["action"] = "Client"
+        kval["action"] = "Connect"
+        kval["clientid"] = idx
+        buildobj["clientid"] = idx
+        # self.clients[tokens[2]] = dict()  # unknown
+        # no msg
+    elif action == "ClientBegin":
+        buildobj["action"] = "Client"
+        kval["action"] = "Begin"
+        kval["clientid"] = idx
+        buildobj["clientid"] = idx
+    elif action == "ClientDisconnect":
+        buildobj["action"] = "Client"
+        kval["action"] = "Disconnect"
+        kval["clientid"] = idx
+        buildobj["clientid"] = idx
+    elif action == "ClientUserinfoChanged":
+        buildobj["action"] = "Client"
+        kval.update(parse_combined_line(tokens[3:]))
+        kval["action"] = "InfoChanged"
+        kval["clientid"] = idx
+        buildobj["clientid"] = idx
+    elif action == "score":
+        buildobj["action"] = "Score"
+        kval.update(parse_scores(tokens[1:]))
+    elif action == "InitGame":
+        kval.update(parse_combined_line(tokens[2:]))
+    elif action == "Kill":
+        kval["clientid"] = idx
+        kval["targetid"] = tokens[3]
+        kval["methodid"] = tokens[4][:-1]  # strip trailing ":"
+        kval["n"] = tokens[5]
+        kval["targetn"] = tokens[7]
+        kval["method"] = tokens[9]
+    elif action == "Exit":
+        kval["reason"] = " ".join(tokens[2:])
+    elif action == "Server":
+        kval["mapname"] = idx
+    elif action in ("ShutdownGame", ):  # other accepted items
+        pass
+    else:
+        logger.debug(f"Not sending {action}")
+        return None
+
+    buildobj["content"] = json.dumps(kval).encode('utf-8')
+    return buildobj
+
+def on_connect(client, userdata, flags, rc):
+    logger.info("Connected with result code "+str(rc))
+    client.subscribe("q3bot/#")
+
+
+def on_message(client, userdata, msg):
+    global SHUTDOWN
+    tokens = msg.topic.split("/")
+    if tokens[0] != "q3bot":
+        logger.info("q3bot", msg.topic+" "+str(msg.payload))
+        return
+    
+    if tokens[1] == "shutdown":
+        logger.info("Got shutdown signal")
+        SHUTDOWN = True
+    else:
+        logger.info("q3bot", msg.topic+" "+str(msg.payload))
+
+
+def on_log(client,userdata,level,buff):
+    console.debug(buff)
+
+
+def main():
+    src = mqtt.Client("q3bot")
+    src.on_connect = on_connect
+    src.on_message = on_message
+    src.enable_logger(logger)
+    src.connect(MQTTSERVER)
+
+    src.loop_start()
+    src.publish("q3server/status", "hello", retain=True)
+    src.will_set("q3server/status", "offline", retain=True)
+    i = 0
+    for line in handle_log():
+        obj = parse_line(line)
+        if obj is None:
+            continue
+        logger.info(f"Publishing {obj}")
+        path = f"q3server/log/{obj['action']}"
+        if "clientid" in obj:
+            path += f"/{obj['clientid']}"
+        if "content" in obj:
+            res = src.publish(path, obj["content"], qos=2)
+            res.wait_for_publish()
+        else:
+            logger.error(f"No content in {obj}")
+        if SHUTDOWN:
+            break
+
+    src.loop_stop()
+
+
+if __name__ == "__main__":
+    main()
